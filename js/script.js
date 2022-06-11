f69ccab2c8e0839f9867758a7a78e80fbbf3c084
Sedrak Qocharyan <sedrakqocharyan@gmail.com>
2022-06-11 15:49:56 +0400

    aaaaa

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-alert("uhd[");
+
+
+
+// const arr = ["anis",'John',"Anna","Vagi","Serdab"]
+
+// console.log(arr.every(function(name){
+//     return name.length ===  4
+// }))
+
+
+
+  const bens = document.querySelectorAll("button");
+    console.log(Array.from(bens).every(function(btn){
+        return btn.classList.contains("btn")
+    }))
+
+
+
